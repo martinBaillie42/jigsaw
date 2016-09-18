2430f7cf3ad5ab1e2dcdd659f47cb931565e347e
martinBaillie42 <martin@martinbaillie.net>
2016-09-18 11:10:11 +0100

    Added note for how to deal with SVGs in <= IE8

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,3 +2,9 @@
  * Created by Martin on 13/09/2016.
  */
 'use strict';
+
+/* See here https://css-tricks.com/using-svg/ for how to use modernizr to provide fallback for svgs in <= IE8
+ if (!Modernizr.svg) {
+ $(".logo img").attr("src", "images/logo.png");
+ }
+ */
